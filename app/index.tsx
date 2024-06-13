d2c5069170fb4cf90f4cd3a1bc686f463e2df964
Nagy Szili <nagy.szilard@kadeno.ro>
2024-06-13 17:28:14 +0300

    init react native expo project

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
